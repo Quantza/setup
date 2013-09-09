87245a3ea299d1a2eb664e38c7cd9d023844bc9b
Luther P <post2base@outlook.com>
2013-09-09 00:00:56 +0000

    Removed other references to .emacs file, and added check for .tmux directory

--- setup.sh
+++ setup.sh
@@ -27,6 +27,9 @@ sudo add-apt-repository -y ppa:cassou/emacs
 sudo apt-get -qq update
 sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
 
+#Install tmux
+sudo apt-get install -y tmux
+
 # Install Heroku toolbelt
 # https://toolbelt.heroku.com/debian
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
