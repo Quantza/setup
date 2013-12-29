193b7b45058212f37c0da6023232143c94877515
Luther P <post2base@outlook.com>
2013-12-29 14:10:55 +0000

    Updated dotfiles

--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -6,19 +6,34 @@ cd $HOME
 if [ -d ./dotfiles/ ]; then
     mv dotfiles dotfiles.old
 fi
+
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
+
 if [ -d .tmux ]; then
     mv .tmux .tmux~
 fi
+
+if [ -d .vagrant.d/ ]; then
+    mv .vagrant.d .vagrant.d~
+fi
+
+git config --global user.name "Quantza"
+git config --global user.email "post2base@outlook.com"
+
 git clone git@github.com:Quantza/dotfiles.git
 ln -sb dotfiles/.screenrc .
-ln -sb dotfiles/.tmux .
+ln -sb dotfiles/.tmux.conf .
 ln -sb dotfiles/.gitmessage.txt .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
+ln -sb dotfiles/tools.sh .
 ln -sf dotfiles/.emacs.d .
 ln -sf dotfiles/.tmux .
+ln -sf dotfiles/.tools .
+ln -sf dotfiles/.vagrant.d .
+
+chmod -R 0700 ~/dotfiles/.tools/
 
