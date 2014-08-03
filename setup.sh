5a2fc8384301e1a1588c4a1cb52790147cdfaa3b
Quantza <post2base@outlook.com>
2014-08-03 09:45:35 +0100

    Removed emacs sources from setup.sh

--- setup.sh
+++ setup.sh
@@ -26,8 +26,8 @@ sudo apt-get install -y rlwrap
 
 # Install emacs24
 # https://launchpad.net/~cassou/+archive/emacs
-sudo add-apt-repository -y ppa:cassou/emacs
-sudo apt-get -qq update
+#sudo add-apt-repository -y ppa:cassou/emacs
+#sudo apt-get -qq update
 sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
 
 #Install tmux
