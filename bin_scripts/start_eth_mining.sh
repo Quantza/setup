b9ed20bd18aab98ef4076709f8ede4d407ca0788
Quantza <post2base@outlook.com>
2015-08-01 15:28:07 +0100

    Updated Scripts

--- bin_scripts/start_eth_mining.sh
+++ bin_scripts/start_eth_mining.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
-THREAD_COUNT=1
-
-#num of input arguments
-# $1 = path to genesis block
-# $2 = number of threads to use for mining.
-
 if [ $# -lt "1" ]
 	then
 	    echo "Not enough arguments supplied (Provide genesis block). Exiting..."
 		exit 1
-elif [ $# -eq "1" ]
-	then
-	    echo "Using 1 thread to mine."
-else
-	THREAD_COUNT="$2"
-	echo "Using ""$THREAD_COUNT"" threads to mine."
 fi
 
+echo "Using gpu to mine."
 eth --frontier -b --genesis "$1" -i -m on --G 2>>$HOME/logs/eth_mine.log
 
--- bin_scripts/start_eth_mining_with_geth.sh
+++ bin_scripts/start_eth_mining_with_geth.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 
-THREAD_COUNT=1
-
-#num of input arguments
-# $1 = path to genesis block
-# $2 = number of threads to use for mining.
-
 if [ $# -lt "1" ]
 	then
 	    echo "Not enough arguments supplied (Provide genesis block). Exiting..."
 		exit 1
-elif [ $# -eq "1" ]
-	then
-	    echo "Using 1 thread to mine."
-else
-	THREAD_COUNT="$2"
-	echo "Using ""$THREAD_COUNT"" threads to mine."
 fi
+
+echo "Using gpu to mine."
 
 geth --genesis "$1" --rpccorsdomain localhost 2>>$HOME/logs/geth_console.log &
 ethminer -G 2>>$HOME/logs/eth_mine.log
