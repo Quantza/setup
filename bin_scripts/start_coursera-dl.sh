d6b3febc637cf7b18e265b055ef3c81c29185329
Quantza <post2base@outlook.com>
2015-08-03 00:13:27 +0100

    Added python install.

--- bin_scripts/start_coursera-dl.sh
+++ bin_scripts/start_coursera-dl.sh
@@ -1,6 +1,6 @@
-if [ $# -lt "1" ]
+if [ $# -lt "2" ]
 	then
-	    echo "Not enough arguments supplied (Provide course list). Exiting..."
+	    echo "Not enough arguments supplied (Provide password, then course list). Exiting..."
 		exit 1
 fi
 
@@ -8,7 +8,7 @@ workon venv_python2
 pip install -U coursera-dl
 
 echo Starting...
-coursera-dl -u post2base@hotmail.co.uk -p phoenix13 -d $HOME/courseraDL/ $1
+coursera-dl -u post2base@hotmail.co.uk -p "$1" -d $HOME/courseraDL/ "$2"
 deactivate
 echo Done!
 
