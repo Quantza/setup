5e8462f9315bbfe05924a6dad13266b427f3a664
Quantza <post2base@outlook.com>
2016-05-19 01:32:25 +0100

    Added mist wallet starter script.

--- bin_scripts/start_mist_server.sh
+++ bin_scripts/start_mist_server.sh
@@ -1,1 +1,1 @@
-x-terminal-emulator -e "cd '$MY_GIT_REPO_DIR'/mist/interface && meteor && read -p 'Press Return to continue'
+x-terminal-emulator -e "cd '$MY_GIT_REPO_DIR'/mist/interface && meteor && read -p 'Press Return to continue'"
