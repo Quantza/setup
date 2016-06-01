49e74aeccf818fddca6140d25fac37dc24f61ada
Quantza <post2base@outlook.com>
2016-06-01 12:29:54 +0100

    Added new PKG prefixes

--- bin_scripts/autoinstall_cuda.sh
+++ bin_scripts/autoinstall_cuda.sh
@@ -16,21 +16,22 @@ fi
 
 cd $CUDA75_DIR
 
-# Install
-OS_NAME_TMP="ubuntu1404"
-ARCH_TMP="x86_64"
-REPO_SUFFIX_TMP="ubuntu1404_7.5-18_amd64"
-CUDAPKG_NAME_TMP="cuda-repo-""$REPO_SUFFIX_TMP"".deb"
-wget http://developer.download.nvidia.com/compute/cuda/repos/"$OS_NAME"/"$ARCH_TMP"/"$CUDAPKG_NAME_TMP"
-sudo dpkg -i "$CUDAPKG_NAME_TMP"
-
-'''
-wget -v http://developer.download.nvidia.com/compute/cuda/7.5/Prod/local_installers/cuda_7.5.18_linux.run
-chmod a+x ./cuda_7.5.18_linux.run
-sudo ./cuda_7.5.18_linux.run
-
 if [ "$DISTRO_ID" == "ubuntu" ]; then
-	$PKG_INSTALL_PREFIX ocl-icd-opencl-dev python-pycuda python3-pycuda
-fi
+	# Install
+	OS_NAME_TMP="ubuntu1404"
+	ARCH_TMP="x86_64"
+	REPO_SUFFIX_TMP="ubuntu1404_7.5-18_amd64"
+	CUDAPKG_NAME_TMP="cuda-repo-""$REPO_SUFFIX_TMP"".deb"
+	wget http://developer.download.nvidia.com/compute/cuda/repos/"$OS_NAME"/"$ARCH_TMP"/"$CUDAPKG_NAME_TMP"
+	$PKG_INSTALL_DEB_PREFIX "$CUDAPKG_NAME_TMP"
+	$PKG_REFRESH_PREFIX
+	$PKG_INSTALL_PREFIX cuda
+else
+	wget -v http://developer.download.nvidia.com/compute/cuda/7.5/Prod/local_installers/cuda_7.5.18_linux.run
+	chmod a+x ./cuda_7.5.18_linux.run
+	sudo ./cuda_7.5.18_linux.run
 
-'''
+	if [ "$DISTRO_ID" == "ubuntu" ]; then
+		$PKG_INSTALL_PREFIX ocl-icd-opencl-dev python-pycuda python3-pycuda
+	fi
+fi
