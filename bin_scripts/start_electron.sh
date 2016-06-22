4ad300e23c8986496177a0578e97ba33e8747117
Quantza <post2base@outlook.com>
2016-06-22 11:55:00 +0100

    Added gdrive scripts

--- bin_scripts/start_electron.sh
+++ bin_scripts/start_electron.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+OLDDIR="$PWD"
+MIST_DIR="$HOME""/GitRepos/mist"
+cd "$MIST_DIR"
+
 electron . --mode mist -- --datadir "/media/quantza-lab/OS/Users/Quantza/AppData/Roaming/Ethereum"
 echo "Exited."
+
+cd "$OLDDIR"
--- bin_scripts/start_electron_console_genesis.sh
+++ bin_scripts/start_electron_console_genesis.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+OLDDIR="$PWD"
+MIST_DIR="$HOME""/GitRepos/mist"
+cd "$MIST_DIR"
+
 # num of input arguments
 if [ $# -eq 0 ]
 	then
@@ -8,3 +12,5 @@ if [ $# -eq 0 ]
 else
 	electron . --mode mist -- --genesis "$1" console 2>>$HOME/logs/geth_console.log
 fi
+
+cd "$OLDDIR"
