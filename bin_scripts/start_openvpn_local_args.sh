b7cb71ff967086ccad88120491b1baef8731cfa8
Quantza <post2base@outlook.com>
2016-08-07 15:44:32 +0100

    Added AWS config.

--- bin_scripts/start_openvpn_local_args.sh
+++ bin_scripts/start_openvpn_local_args.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 OLDDIR="$PWD"
-cd "$HOME"/.vpn && sudo openvpn "$HOME"/.vpn/client.conf --ca "$1" --crl-verify "$2"
+cd "$HOME"/.vpn && sudo openvpn --config "$HOME"/.vpn/client.conf --ca "$1" --crl-verify "$2"
 cd "$OLDDIR"
 
 echo "Exited."
